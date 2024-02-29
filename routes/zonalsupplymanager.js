0328ffd524ff1fc4450b9940f52a7cb71c242474
Suprio85 <2105085@ugrad.cse.buet.ac.bd>
2024-02-29 23:13:01 +0600

    zsm updated

--- routes/zonalsupplymanager.js
+++ routes/zonalsupplymanager.js
@@ -90,18 +90,30 @@ router.post('/totalproductregion', async function (req, res) {
     console.log(regionId);
     console.log(quantity);
 
+    const connection = await oracledb.getConnection(dbConfig);
     try {
-        const connection = await oracledb.getConnection(dbConfig);
 
-        const query = `UPDATE PRODUCT_ALLOTMENT SET QUANTITY = :quantity WHERE PRODUCT_ID = :productId AND REGION_ID = :regionId`;
+        const query = `
+  UPDATE PRODUCT_ALLOTEMENT
+  SET AMOUNT = :quantity, STATUS = 'ON PROCESS'
+  WHERE PRODUCT_ID = :productId AND REGION_ID = :regionId
+`;
 
-        const result = await connection.execute(query, { productId, regionId, quantity });
+        const binds = {
+            quantity: { dir: oracledb.BIND_IN, val: quantity },
+            productId: { dir: oracledb.BIND_IN, val: productId },
+            regionId: { dir: oracledb.BIND_IN, val: regionId },
+        };
+
+
+  result =  await connection.execute(query, binds);
         console.log(result);
 
         res.json({ success: true, message: 'Product allotted successfully.' });
-
+        await connection.commit();
 
     } catch (error) {
+        await connection.rollback();
         console.error('Error allotting product:', error);
         res.status(500).json({ success: false, message: 'Internal server error.' });
     }
@@ -110,18 +122,71 @@ router.post('/totalproductregion', async function (req, res) {
 router.get('/makeold', async function (req, res, next) {
     console.log("in makeold");
 
+    const connection = await oracledb.getConnection(dbConfig);
     res.json({ success: true, message: 'Data received and processed successfully' });
     try {
-        const connection = await oracledb.getConnection(dbConfig);
 
         const query = `UPDATE PRODUCT_ALLOTEMENT SET STATUS = 'OLD'`
         const result = await connection.execute(query);
-        
+
+
+
         //console.log("hi");
-     
+        await connection.commit();
     } catch (error) {
+        await connection.rollback();
         console.error('Error fetching data from the database:', error);
 
+    }
+})
+
+
+router.get('/sendregion', async function (req, res, next) {
+    console.log("in sendregion");
+    try {
+        const connection = await oracledb.getConnection(dbConfig);
+        const query = `SELECT * FROM REGION`;
+        const result = await connection.execute(query);
+        let regions = [];
+        for (const row of result.rows) {
+            let region = {
+                id: row[0],
+                name: row[1]
+            };
+
+            regions.push(region);
+        }
+
+
+        console.log(regions);
+        res.json({ success: true, regions: regions });
+    } catch (error) {
+        console.error('Error fetching data from the database:', error);
+        res.status(500).json({ success: false, message: 'Error fetching data from the database' });
+    }
+})
+
+router.post('/confirmregion', async function (req, res) {
+    console.log(req.body);
+
+    const { regionId } = req.body;
+
+    console.log(regionId);
+    const connection = await oracledb.getConnection(dbConfig);
+
+    try {
+        const query = `UPDATE PRODUCT_ALLOTEMENT SET STATUS = 'UPDATED' WHERE REGION_ID = :regionId`;
+
+        const result = await connection.execute(query, { regionId });
+        res.json({ success: true, message: 'Region confirmed successfully' });
+
+        await connection.commit();
+        await connection.close();
+
+    } catch (error) {
+        await connection.rollback();
+        console.error('Error confirming region:', error);
+        res.status(500).json({ success: false, message: 'Internal server error.' });
     }
 })
 

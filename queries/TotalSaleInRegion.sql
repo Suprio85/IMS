f9d9b87ad5378f838bb145deb14021e175bbcc08
Suprio85 <2105085@ugrad.cse.buet.ac.bd>
2024-03-09 14:22:39 +0600

    Totalsaleinregion fixed

--- queries/TotalSaleInRegion.sql
+++ queries/TotalSaleInRegion.sql
@@ -8,7 +8,7 @@ CREATE OR REPLACE FUNCTION get_total_quantity(
 IS
     v_total_quantity NUMBER;
 BEGIN
-    SELECT NVL(SUM(pp.quantity), 0)
+    SELECT SUM(pp.quantity)
     INTO v_total_quantity
     FROM PURCHASED_PRODUCT pp
     LEFT JOIN PURCHASE p ON pp.purchase_id = p.purchase_id
@@ -16,11 +16,10 @@ BEGIN
     LEFT JOIN AREAS a ON a.AREA_CODE = s.AREA_CODE
     WHERE pp.PRODUCT_ID = p_product_id
       AND a.REGION_ID = p_region_id
-			 AND TO_CHAR(p.PURCHASE_TIME, 'YYYY-MM') BETWEEN TO_CHAR(start_month, 'YYYY-MM') AND TO_CHAR(end_month, 'YYYY-MM'); 
+			 AND TO_CHAR(p.PURCHASE_TIME, 'YYYY-MM') BETWEEN start_month AND end_month;
   
     RETURN NVL(v_total_quantity, 0);
 EXCEPTION
     WHEN NO_DATA_FOUND THEN
         RETURN 0;
-END get_total_quantity; 
--- FOR OWNER PRODUCT VS REGION GRAPH;
+END get_total_quantity;
